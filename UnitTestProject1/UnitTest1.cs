2b2c9cd961726f170efbd95ef1688c5b5af6864e
smittal <smittal@L01LTP0051>
2018-06-04 13:43:38 +0530

    Test1

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfServiceLibrary1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IService1 wcfserv = new WcfServiceLibrary1.Service1();
+
+            Assert.AreEqual(wcfserv.GetData(2), "You entered: 2");
+        }
+    }
+}
